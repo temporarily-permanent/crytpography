5b4ba8f716aeb5d3e3430ce48bb75ef1d0ae4c48
Jannes <146443595+temporarily-permanent@users.noreply.github.com>
2025-09-11 07:28:54 +0200

    got testing working

---
+++ Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        public static int Add(int a, int b) => a + b;
+    }
+}
+
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Calculator;
+
+namespace TestProject1;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.That(Calculator.Program.Add(5,5), Is.EqualTo(10));
+    }
+}
