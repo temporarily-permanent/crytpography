788bfe2978d494cbae6f9f8a9db40c70a4f1ffd5
Jannes <37171@ma-web.nl>
2025-09-29 14:20:31 +0200

    finished and tested SHA1 implementation after dyslexic moment

--- Calculator/Cryptography/SHA-1.cs
+++ Calculator/Cryptography/SHA-1.cs
@@ -51,36 +51,52 @@ public class SHA_1
 			{
 				T = uint.RotateLeft(a,5) + Parity(b,c,d) + e + K4 + messageSchedule[j];
 				e = d; d = c; c = uint.RotateLeft(b, 30); b = a; a = T;
-				Console.WriteLine(j);
 			}
 
 			//compute intermediate hash
 			H0 = a + H0; H1 = b + H1; H2 = c + H2; H3 = d + H3; H4 = e + H4;
 		}
 		
-		uint[] output = new uint[5]{H0, H1, H2, H3, H4};
+		uint[] output = {H0, H1, H2, H3, H4};
 		return string.Concat(output.Select(x => x.ToString("X8")));
-		byte[] bytes = new byte[20];
+	}
 
-		for (int i = 0; i < output.Length; i++)
-		{
-			byte[] temp = BitConverter.GetBytes(output[i]);
-			if (BitConverter.IsLittleEndian)
-			{
-				Array.Reverse(temp);
-			} // fix endian
-
-			Buffer.BlockCopy(temp, 0, bytes, i * 4, 4);
+	public static string SHA256(List<byte> Input)
+	{
+		/*const uint[] K = {
+			0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+			0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+			0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+			0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+			0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+			0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+			0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+			0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+			0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+			0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+			0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+			0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+			0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+			0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+			0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+			0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 		}
+		*/
 		
-		return BitConverter.ToString(bytes).Replace("-", string.Empty);
-		//return H0.ToString() + H1.ToString() + H2.ToString() + H3.ToString() + H4.ToString();
+		uint H0 = 0x6a09e667;
+		uint H1 = 0xbb67ae85;
+		uint H2 = 0x3c6ef372;
+		uint H3 = 0xa54ff53a;
+		uint H4 = 0x510e527f;
+		uint H5 = 0x9b05688c;
+		uint H6 = 0x1f83d9ab;
+		uint H7 = 0x5be0cd19;
 		throw new NotImplementedException();
 	}
 
 	private static uint Ch(uint a, uint b, uint c)
 	{
-		return (a & b) ^ (~b & c);
+		return (a & b) ^ (~a & c);
 	}
 	private static uint Parity(uint a, uint b, uint c)
 	{
