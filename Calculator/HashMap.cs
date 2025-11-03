28b6cd3f8630639bdefb8b84d9f25becbbb98a0d
Jannes <37171@ma-web.nl>
2025-11-03 14:55:36 +0100

    finished writing add item to hash table, nothing tested yet

--- Calculator/HashMap.cs
+++ Calculator/HashMap.cs
@@ -3,19 +3,30 @@ using System.Text;
 using Calculator.Cryptography;
 
 namespace Calculator;
-public class HashMap<TKey, TValue>
+public class HashMap<TKey, TValue> 
 {
 	
 	private TKey[] keys;
 	private TValue[] values;
-
+	
+	
 	private int size;
 	//public Dictionary<char, int> map = new Dictionary<char, int>();
 	public HashMap(int capacity)
 	{
+		try
+		{
+			
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e);
+			throw;
+		}
 		size = capacity;
 		keys = new TKey[capacity];
 		values = new TValue[capacity];
+		
 	}
 
 	public void Add(TKey key, TValue value)
@@ -24,14 +35,26 @@ public class HashMap<TKey, TValue>
 		string hash = SHA_1.SHA1(Encoding.UTF8.GetBytes(key.ToString()).ToList());
 		BigInteger.TryParse(hash, out BigInteger bigInt);
 		int index = (int)(bigInt % size);
+
 		
-		//todo occupancy check
+		// check for collision
+		if (EqualityComparer<TKey>.Default.Equals(keys[index], key))//keys[index] != key)
+		{
+			// search for available room
+			index++;
+			while (keys[index] != null)
+			{
+				index++;
+			}
+			
+			keys[index] = key;
+			values[index] = value;
+			
+			return;
+		}
 		
 		values[index] = value;
-		
-		//todo resolve
-		
-		throw new NotImplementedException();
+		keys[index] = key;
 	}
 
 	public TValue Get(TKey key)
